4c64e781d98b02bdc650715000ec5a79947c01e7
Sebastian Masternak <smasternak3@gmail.com>
2025-12-13 20:49:22 +0100

    Update Card.cs

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -1,12 +1,7 @@
 using UnityEngine;
 
 public class Card : MonoBehaviour
-{
-    public string Name;
-    public string Description;
-    public int Price;
-    public GameObject cardTexture;
-    public GameObject objectRelated;
+{ 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
